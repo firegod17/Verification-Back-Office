ae4ef19f1f527c0c8a0024aa6810faad8dfaeba3
firegod17 <fire.god.47@gmail.com>
2019-08-15 16:19:20 +0300

    1

---
+++ src/app/employees/employees.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { RouteStateService } from '../core/services/route-state.service';
+import { EmployeeDataService } from './employee-data.service';
+
+@Component({
+  selector: 'app-employees',
+  templateUrl: './employees.component.html',
+  styleUrls: ['./employees.component.css']
+})
+export class EmployeesComponent implements OnInit {
+  columns: any[];
+
+  employees: any[];
+
+  pageSize: number;
+
+  constructor(
+    private routeStateService: RouteStateService,
+    private employeeService: EmployeeDataService) { }
+
+  ngOnInit() {
+
+    this.pageSize = 10;
+
+    this.columns = [
+      { field: 'Name', header: 'Name' },
+      { field: 'Department', header: 'Department' },
+      { field: 'Address', header: 'Address' },
+      { field: 'Age', header: 'Age' }
+    ];
+
+    this.employees = this.employeeService.getEmployeeList();
+  }
+
+  goToDepartmentDetails(department: number) {
+    this.routeStateService.add("Department details", "/main/departments/department-detail", department, false);
+  }
+}
